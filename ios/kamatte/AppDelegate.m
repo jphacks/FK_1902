659173b66187a34cb60ecec6b4b97d1975c455be
ryo.watanabe <susiyaki.dev@gmail.com>
2019-10-16 20:53:10 +0900

    firestore設定完了

--- ios/kamatte/AppDelegate.m
+++ ios/kamatte/AppDelegate.m
@@ -17,6 +17,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [FIRApp configure];
+  FIRFirestore *defaultFirestore = [FIRFirestore firestore];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"kamatte"
--- src/components/pages/sample.js
+++ src/components/pages/sample.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { Actions } from "react-native-router-flux";
-
 import { View, Text } from "react-native";
 
+import { db } from "app/src/utils/firebase";
+
 export default class extends React.Component {
+  // componentDidMount() {
+  //   db.collection("sample")
+  //     .add({ aaa: "aaa" })
+  //     .then(() => console.log("ok"))
+  //     .catch(e => console.log("error"));
+  // }
+
   render() {
     return (
       <View>
