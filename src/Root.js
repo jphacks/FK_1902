f8c8972895239d805af79137792c30a3af78ae1d
ryo.watanabe <susiyaki.dev@gmail.com>
2019-10-16 20:32:00 +0900

    routerサンプル作成

---
+++ src/Root.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Router, Stack, Scene } from "react-native-router-flux";
+
+import sample from "app/src/components/pages/sample";
+import sample2 from "app/src/components/pages/sample2";
+
+const Root = () => {
+  return (
+    <Router>
+      <Stack key="root">
+        <Scene key="sample" component={sample} title="sample" />
+        <Scene key="sample2" component={sample2} title="sample2" />
+      </Stack>
+    </Router>
+  );
+};
+
+export default Root;
