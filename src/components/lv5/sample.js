14e4d9d133c2485912a45eaa482f1f17a27ebd67
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-10-17 20:19:52 +0900

    delete import from firebase

--- src/components/lv5/sample.js
+++ src/components/lv5/sample.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Actions } from "react-native-router-flux";
 import { View, Text } from "react-native";
-import { storage } from "app/src/utils/firebase";
-
-import { db } from "app/src/utils/firebase";
 
 export default class extends React.Component {
   render() {
