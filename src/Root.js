8f434f352d3be57381e13b9502e4803509b691ad
ryo.watanabe <susiyaki.dev@gmail.com>
2019-10-17 18:14:32 +0900

    sampleコンポーネント作成

--- src/Root.js
+++ src/Root.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Router, Stack, Scene } from "react-native-router-flux";
 
 import sample from "app/src/components/pages/sample";
-import sample2 from "app/src/components/pages/sample2";
+import chatSample from "app/src/components/pages/chatSample";
 
 const Root = () => {
   return (
     <Router>
       <Stack key="root">
         <Scene key="sample" component={sample} title="sample" />
-        <Scene key="sample2" component={sample2} title="sample2" />
+        <Scene key="chatSample" component={chatSample} title="chatroom" />
       </Stack>
     </Router>
   );
--- src/components/pages/chatSample.js
+++ src/components/pages/chatSample.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { Actions } from "react-native-router-flux";
-
 import { View, Text } from "react-native";
 
 export default class extends React.Component {
   render() {
     return (
       <View>
-        <Text>sample2 text</Text>
-        <Text>sample2 text</Text>
-        <Text>sample2 text</Text>
-        <Text>sample2 text</Text>
-        <Text>sample2 text</Text>
+        <Text>aaa</Text>
         <Text onPress={() => Actions.sample()}>sampleへ</Text>
       </View>
     );
