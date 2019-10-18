0cc157650e71d4c5910534052d3f9a70bbc1c186
ryo.watanabe <susiyaki.dev@gmail.com>
2019-10-18 20:04:48 +0900

    storageRef追加

--- src/models/userDetail.js
+++ src/models/userDetail.js
@@ -1,4 +1,4 @@
-import { db } from "app/src/utils/firebase";
+import { db, storage } from "app/src/utils/firebase";
 import { snapShotToArray, documentToObject } from "app/src/models/utils/format";
 
 class UserDetail {
@@ -15,10 +15,11 @@ class UserDetail {
     };
   }
 
-  ref = db.collection("userDetails");
+  dbRef = db.collection("userDetails");
+  avaterStorageRef = storage.ref().child("avater");
 
   getByUserId = async userId => {
-    const document = await this.ref
+    const document = await this.dbRef
       .doc(userId)
       .get()
       .catch(e => console.error(e.message));
@@ -27,11 +28,16 @@ class UserDetail {
 
   set = async (userId, profile) => {
     delete profile.docId;
-    const req = await this.ref.doc(userId).set({ ...profile });
+    const req = await this.dbRef.doc(userId).set({ ...profile });
     return req;
   };
 
-  // createAvater -> storage
+  createAvater = async file => {
+    const req = await avaterStorageRef.put(file).then(snapShot => {
+      console.log(snapShot);
+    });
+    return req;
+  };
 }
 
 export default UserDetail;
