f7f2248e5aebdfc8076bda2e8e4c4332a48cd005
ryo.watanabe <susiyaki.dev@gmail.com>
2019-10-19 17:01:23 +0900

    NavBar作成

--- src/components/lv2/NavBar.js
+++ src/components/lv2/NavBar.js
@@ -6,15 +6,37 @@ import Icon from "app/src/components/lv1/Icon";
 
 const { width, height } = Dimensions.get("window");
 
+// TODO: user情報を受け取ってavatarとcurrency表示
+
 export default () => {
   return (
     <View style={styles.navBar}>
       <View style={styles.avatarBg}>
         <Image style={styles.avatar} />
       </View>
-      <View>
-        <Icon />
-        <Icon />
+      <View style={styles.propertiesWrapper}>
+        <View style={styles.iconWrapper}>
+          <Icon
+            style={styles.icon}
+            size={height * 0.04}
+            name="account-card-details-outline"
+            color={COLOR.white}
+          />
+          <Text style={{ color: COLOR.white, marginLeft: width * 0.01 }}>
+            ×
+          </Text>
+          <Text style={styles.iconLabel}>5</Text>
+        </View>
+        <View style={styles.iconWrapper}>
+          <Icon
+            style={styles.icon}
+            size={height * 0.04}
+            name="flower-tulip"
+            color={COLOR.white}
+          />
+          <Text style={{ color: COLOR.white }}>×</Text>
+          <Text style={styles.iconLabel}>1000</Text>
+        </View>
       </View>
     </View>
   );
@@ -24,15 +46,18 @@ const styles = StyleSheet.create({
   navBar: {
     height: height * 0.13,
     backgroundColor: COLOR.main,
-    justifyContent: "center",
-    paddingTop: height * 0.03
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingTop: height * 0.05,
+    paddingRight: width * 0.05,
+    paddingLeft: width * 0.05
   },
   avatarBg: {
     height: height * 0.06,
     width: height * 0.06,
     borderRadius: 100,
     backgroundColor: COLOR.white,
-    marginLeft: width * 0.05,
     justifyContent: "center",
     alignItems: "center"
   },
@@ -42,5 +67,21 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     backgroundColor: COLOR.gray
   },
-  Icon: {}
+  propertiesWrapper: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  iconWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: width * 0.02
+  },
+  icon: {
+    marginRight: width * 0.01
+  },
+  iconLabel: {
+    color: COLOR.white,
+    fontSize: height * 0.02,
+    marginLeft: width * 0.01
+  }
 });
