fdd5ad90a8a58f8078cf4ead592866759598a217
ryo.watanabe <susiyaki.dev@gmail.com>
2019-10-19 23:54:00 +0900

    loadingをリッチに

--- src/components/lv3/ChatroomList.js
+++ src/components/lv3/ChatroomList.js
@@ -2,9 +2,11 @@ import React from "react";
 import {
   ScrollView,
   RefreshControl,
+  ActivityIndicator,
   View,
   Text,
   Button,
+  Dimensions,
   StyleSheet
 } from "react-native";
 
@@ -13,7 +15,7 @@ import COLOR from "app/src/config/color.json";
 import ChatroomCard from "app/src/components/lv2/ChatroomCard";
 
 export default props => {
-  const { chatrooms, refreshing, onRefresh, onEnterChatroom } = props;
+  const { chatrooms, refreshing, onRefresh, onEnterChatroom, loading } = props;
 
   return (
     <>
@@ -32,18 +34,28 @@ export default props => {
         refreshControl={
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }>
-        {chatrooms.map((chatroom, index) => (
-          <ChatroomCard
-            chatroom={chatroom}
-            key={index}
-            onPress={() => onEnterChatroom(chatroom.docId)}
-          />
-        ))}
+        {loading ? (
+          <View
+            style={{
+              margin: height * 0.2
+            }}>
+            <ActivityIndicator size="large" color={COLOR.accent} />
+          </View>
+        ) : (
+          chatrooms.map((chatroom, index) => (
+            <ChatroomCard
+              chatroom={chatroom}
+              key={index}
+              onPress={() => onEnterChatroom(chatroom.docId)}
+            />
+          ))
+        )}
       </ScrollView>
     </>
   );
 };
 
+const { width, height } = Dimensions.get("window");
 const styles = StyleSheet.create({
   listBg: {
     height: "85%",
--- src/components/lv4/ChatroomIndex.js
+++ src/components/lv4/ChatroomIndex.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
 
 import ChatroomList from "app/src/components/lv3/ChatroomList";
 
 export default props => {
-  const { toRegister, loading } = props;
-
-  return (
-    <View>{loading ? <Text>loading</Text> : <ChatroomList {...props} />}</View>
-  );
+  return <ChatroomList {...props} />;
 };
