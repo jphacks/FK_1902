df3dad987f6bcbff1dfb696a049bd89a3bf23e9b
ryo.watanabe <susiyaki.dev@gmail.com>
2019-10-20 23:37:45 +0900

    setting profile調整

--- src/components/lv4/SettingProfile.js
+++ src/components/lv4/SettingProfile.js
@@ -4,70 +4,93 @@ import constantsToPickerOptions from "app/src/utils/constantsToPickerOptions";
 import USER from "app/src/config/user.json";
 import COLOR from "app/src/config/color";
 
-import { View, Text, Image } from "react-native";
-import Input from "app/src/components/lv1/Input";
+import { View, Dimensions, Image, StyleSheet } from "react-native";
+import InputWithIcon from "app/src/components/lv2/InputWithIcon";
+import LoadingOverlay from "app/src/components/lv2/LoadingOverlay";
 import Picker from "app/src/components/lv1/Picker";
 import Button from "app/src/components/lv1/Button";
 
 export default props => {
   const {
     profile,
-    onChangeProfileText,
+    onChangeText,
     onSelectPicker,
     selectAvatar,
-    imageUpload,
     onUpdate,
-    onSignOut
+    onSignOut,
+    loading
   } = props;
 
   return (
-    <View>
-      <Text>ユーザーネーム</Text>
-      <Input
-        placeholder="ユーザーネームを入力してください"
-        value={profile.name}
-        onChangeText={text => this.onChangeProfileText("name", text)}
-      />
-      <Text>年齢</Text>
-      <Picker
-        value={profile.age}
-        options={constantsToPickerOptions(USER.age)}
-        onValueChange={value => onSelectPicker("age", value)}
-      />
-      <Text>性別</Text>
-      <Picker
-        value={profile.gender}
-        options={constantsToPickerOptions(USER.gender)}
-        onValueChange={value => onSelectPicker("gender", value)}
-      />
-      {/* <Text>現在のプロフィール写真</Text>
-      <Image
-        source={{ uri: profile.avatar }}
-        style={{ height: 100, minWidth: 100 }}
-      /> */}
-
-      {/* <Button
-        title="新しいプロフィール画像選択"
-        onPress={selectAvatar}
-        backgroundColor={COLOR.main}
-      />
-      <Button
-        title="プロフィール画像更新"
-        onPress={imageUpload}
-        backgroundColor={COLOR.main}
-      /> */}
-      <Button
-        title="決定"
-        onPress={onUpdate}
-        backgroundColor={COLOR.main}
-        style={{ marginBottom: 24 }}
-      />
-      <Button
-        title="ログアウト"
-        onPress={onSignOut}
-        bgColor={COLOR.black}
-        backgroundColor={COLOR.black}
-      />
-    </View>
+    <>
+      <LoadingOverlay loading={loading} />
+      <View>
+        <View style={styles.avatarWrapper}>
+          <Image style={styles.avatar} source={{ uri: profile.avatar }} />
+          <Button
+            small
+            title="プロフィール画像選択"
+            style={styles.avatarSelect}
+            onPress={selectAvatar}
+          />
+        </View>
+        <View style={styles.inputs}>
+          <InputWithIcon
+            placeholder="ユーザーネームを入力してください"
+            iconName="account"
+            value={profile.name}
+            onChangeText={text => onChangeText("name", text)}
+          />
+          <Picker
+            value={profile.age}
+            options={constantsToPickerOptions(USER.age)}
+            onValueChange={value => onSelectPicker("age", value)}
+          />
+          <Picker
+            value={profile.gender}
+            options={constantsToPickerOptions(USER.gender)}
+            onValueChange={value => onSelectPicker("gender", value)}
+          />
+        </View>
+        <View style={styles.actions}>
+          <Button
+            title="設定して戻る"
+            onPress={onUpdate}
+            bgColor={COLOR.main}
+            style={{ marginBottom: 24 }}
+          />
+          <Button
+            title="ログアウト"
+            onPress={onSignOut}
+            bgColor={COLOR.black}
+          />
+        </View>
+      </View>
+    </>
   );
 };
+
+const { height, width } = Dimensions.get("window");
+const styles = StyleSheet.create({
+  avatarWrapper: {
+    alignItems: "center",
+    height: height * 0.3,
+    padding: 5
+  },
+  avatar: {
+    height: height * 0.2,
+    width: height * 0.2,
+    borderRadius: 100,
+    marginBottom: height * 0.03,
+    backgroundColor: COLOR.gray
+  },
+  avatarSelect: {
+    height: height * 1.97
+  },
+  inputs: {
+    height: height * 0.4
+  },
+  actions: {
+    height: height * 0.3
+  }
+});
